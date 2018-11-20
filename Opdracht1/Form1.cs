30f0a3aa00adc01d972128dbdce0ba7d924a1a78
JeBoyJJ <45012296+JeBoyJJ@users.noreply.github.com>
2018-11-20 17:01:46 +0100

    Update Form1.cs

--- Opdracht1/Form1.cs
+++ Opdracht1/Form1.cs
@@ -6,15 +6,62 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Opdracht1 {
     public partial class MainScreen : Form {
         public MainScreen() {
             InitializeComponent();
+
+
+            this.WindowState = FormWindowState.Maximized;
+
+            this.Load += FormLoad;
         }
 
+        private void FormLoad(object sender, EventArgs e)
+        {
+            int x = this.ClientSize.Width / 2 - this.centerButton.Size.Width / 2;
+            int y = this.ClientSize.Height / 2 - this.centerButton.Size.Height / 2;
+            this.centerButton.Location = new Point(x, y);
+            this.Controls.Add(centerButton);
+            this.centerButton.Text = "test text";
+
+            this.centerButton.Click += this.OnCenterButtonClick;
+            this.Controls.Add(this.targetButton);
+            this.targetButton.Visible = false;
+            this.targetButton.Click += OnTargetButtonClick; ;
+        }
+
+        private void OnTargetButtonClick(object sender, EventArgs e)
+        {
+
+            this.targetButton.Visible = false;
+            this.centerButton.Visible = true;
+            Console.WriteLine(this.responseTimer.ElapsedMilliseconds);
+            Console.WriteLine(this.targetButton.Size.Width);
+            Console.WriteLine(DistanceCursorToBox());
+        }
+
+        private void OnCenterButtonClick(object sender, EventArgs e)
+        {
+            this.centerButton.Visible = false;
+
+            var newRectangle = RandomRectangleWithinScreen();
+            this.targetButton.Location = newRectangle.Location;
+            this.targetButton.Size = newRectangle.Size;
+            this.targetButton.Visible = true;
+            this.responseTimer.Restart();
+        }
+
+
+        Button centerButton = new Button();
         Random mainRandom = new Random();
+        Button targetButton = new Button();
+        Stopwatch responseTimer = new Stopwatch();
+
+
 
         public static double DistanceCursorToBox(Point cursorLocation, Rectangle box) {
             int horizontalDistance = 0;
@@ -52,11 +99,13 @@ namespace Opdracht1 {
         }
 
         //randomly generates a rectangle that is withing the bounds of the screen
-        public Rectangle RandomRectangleWithinScreen(Random r) {
+        public Rectangle RandomRectangleWithinScreen() {
             const int MAX_RECTANGLE_WIDTH = 300;
             const int MAX_RECTANGLE_HEIGHT = 300;
             const int MIN_RECTANGLE_WIDTH = 100;
             const int MIN_RECTANGLE_HEIGHT = 100;
+
+            Random r = this.mainRandom;
 
             int width = r.Next(MIN_RECTANGLE_WIDTH, MAX_RECTANGLE_WIDTH);
             int height = r.Next(MIN_RECTANGLE_HEIGHT, MAX_RECTANGLE_HEIGHT);
